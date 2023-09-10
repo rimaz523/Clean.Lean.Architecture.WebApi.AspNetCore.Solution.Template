46e0663fa509ade44ccbfd1a8745d7c7f119f03b
Mohommed Rimaz <rimazmohommed523@gmail.com>
2023-09-10 22:15:25 +1200

    initial commit

---
+++ src/WebApi/Controllers/HealthController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HealthController : ControllerBase
+    {
+        private readonly ILogger<HealthController> _logger;
+
+        public HealthController(ILogger<HealthController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "HealthCheck")]
+        public IActionResult Get()
+        {
+            _logger.LogInformation("Polling health check endpoint");
+            return new OkResult();
+        }
+    }
+}
