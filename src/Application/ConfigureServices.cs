498a1211a34434b8fd146802ef99f9dcdaa0cc31
Mohommed Rimaz <rimazmohommed523@gmail.com>
2023-09-21 14:58:23 +1200

    setup fluent validation for template

--- src/Application/ConfigureServices.cs
+++ src/Application/ConfigureServices.cs
@@ -1,4 +1,7 @@
 ﻿using System.Reflection;
+using Application.Common.Behaviours;
+using FluentValidation;
+using MediatR;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Application;
@@ -8,9 +11,11 @@ public static class ConfigureServices
     public static IServiceCollection AddApplication(this IServiceCollection services)
     {
         services.AddAutoMapper(Assembly.GetExecutingAssembly());
+        services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
         services.AddMediatR(cfg =>
         {
             cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
+            cfg.AddBehavior(typeof(IPipelineBehavior<,>), typeof(ValidationBehaviour<,>));
         });
         return services;
     }
---
+++ src/Application/Users/Commands/CreateUser/CreateUserCommandValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+
+namespace Application.Users.Commands.CreateUser;
+public class CreateUserCommandValidator : AbstractValidator<CreateUserCommand>
+{
+    public CreateUserCommandValidator()
+    {
+        RuleFor(createUserCommand => createUserCommand.FirstName)
+        .NotEmpty()
+        .MaximumLength(255);
+        RuleFor(createUserCommand => createUserCommand.LastName)
+        .NotEmpty()
+        .MaximumLength(255);
+    }
+}
