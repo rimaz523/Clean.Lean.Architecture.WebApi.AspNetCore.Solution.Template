fbc299f6ab3d29303289e8bec5517d498e8e9b69
Mohommed Rimaz <rimazmohommed523@gmail.com>
2024-04-01 04:31:38 +1300

    run dotnet format

--- src/WebApi/Program.cs
+++ src/WebApi/Program.cs
@@ -12,7 +12,7 @@ public class Program
         var builder = WebApplication.CreateBuilder(args);
 
         // Add services to the container.
-        builder.Services.AddApplication(); 
+        builder.Services.AddApplication();
         builder.Services.AddInfrastructure(builder.Configuration);
 
         builder.Services.AddControllers();
